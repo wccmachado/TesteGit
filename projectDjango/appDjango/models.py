08dee408db83db366d36d3a02345e5c21173c78a
aluno <wccmachado@gmail.com>
2023-05-19 16:16:22 -0300

    second  commit in model

--- projectDjango/appDjango/models.py
+++ projectDjango/appDjango/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class teste(models.Model):
     nome=models.CharField(max_length=100)
+
+class teste2(models.Model):
+    idade = models.CharField(max_length=100)
